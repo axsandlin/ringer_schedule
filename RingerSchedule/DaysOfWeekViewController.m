de898dfe4d3d92a82398bed9d073d513bb0139e1
Aubri Sandlin <axsandlin@aubri-laptop.local>
2014-01-16 07:57:50 -0800

    fixed untracked files

--- RingerSchedule/DaysOfWeekViewController.m
+++ RingerSchedule/DaysOfWeekViewController.m
@@ -36,6 +36,8 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    
+    [self.navigationItem setHidesBackButton:YES];
 
     // Uncomment the following line to preserve selection between presentations.
     // self.clearsSelectionOnViewWillAppear = NO;
